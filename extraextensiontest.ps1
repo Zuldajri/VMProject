f940742c719288366d8c1a47de5d9e7235547423
Zuldajri <w.matyas15@hotmail.fr>
2020-03-20 12:53:40 +0100

    Update extraextensiontest.ps1

--- extraextensiontest.ps1
+++ extraextensiontest.ps1
@@ -1,1 +1,29 @@
+[CmdletBinding()]
 
+
+
+Param(
+    [string] $VMName, 
+    [string] $GuestOSName,
+    [string] $StorageAccountName,
+    [string] $StorageAccountKey,
+    [string] $USERNAME,
+    [string] $PASSWORD
+ )
+
+
+
+#Initialize Data Disks
+Get-Disk | ` 
+Where partitionstyle -eq 'raw' | ` 
+Initialize-Disk -PartitionStyle GPT -PassThru | ` 
+New-Partition -AssignDriveLetter -UseMaximumSize | ` 
+Format-Volume -FileSystem ReFS -NewFileSystemLabel "datadisk" -Confirm:$false
+
+
+
+
+
+
+
+ 
