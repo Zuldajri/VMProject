dbc0e21bcd8ac34c8ddf80317482c0b2d548c960
Zuldajri <w.matyas15@hotmail.fr>
2021-07-06 18:41:51 +0200

    Update v11powershell.ps1

--- v11powershell.ps1
+++ v11powershell.ps1
@@ -24,7 +24,7 @@ Get-Disk | `
 Where partitionstyle -eq 'raw' | ` 
 Initialize-Disk -PartitionStyle GPT -PassThru | ` 
 New-Partition -AssignDriveLetter -UseMaximumSize | ` 
-Format-Volume -FileSystem ReFS -NewFileSystemLabel "datadisk" -Confirm:$false
+Format-Volume -FileSystem ReFS -NewFileSystemLabel "datadisk" -AllocationUnitSize 65536 -Confirm:$false
 
 
 
