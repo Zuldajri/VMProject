d313f0f3a5e7ff6cb3396d38259871d0b9e6a11e
Zuldajri <w.matyas15@hotmail.fr>
2022-03-15 12:22:28 +0100

    Create extensionwin.ps1

---
+++ extensionwin.ps1
@@ -0,0 +1,33 @@
+[CmdletBinding()]
+
+
+
+Param(
+    [string] $VMName, 
+    [string] $GuestOSName,
+    [string] $StorageAccountName,
+    [string] $StorageAccountKey,
+    [string] $USERNAME,
+    [string] $PASSWORD
+ )
+
+$osDrive = ((Get-WmiObject Win32_OperatingSystem).SystemDrive).TrimEnd(":")
+$size = (Get-Partition -DriveLetter $osDrive).Size
+$maxSize = (Get-PartitionSupportedSize -DriveLetter $osDrive).SizeMax
+if ($size -lt $maxSize){
+     Resize-Partition -DriveLetter $osDrive -Size $maxSize
+}
+
+
+
+#Initialize Data Disks
+Get-Disk | ` 
+Where partitionstyle -eq 'raw' | ` 
+Initialize-Disk -PartitionStyle GPT -PassThru | ` 
+New-Partition -AssignDriveLetter -UseMaximumSize | ` 
+Format-Volume -FileSystem ReFS -NewFileSystemLabel "datadisk" -Confirm:$false
+
+
+
+
+
